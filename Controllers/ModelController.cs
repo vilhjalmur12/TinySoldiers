7c9ba0a9d57ff5228cbcb38df732c304fd7148b4
villi <vilhjalmur12@ru.is>
2018-09-19 12:17:36 +0000

    Model Controller setup

--- Controllers/ModelController.cs
+++ Controllers/ModelController.cs
@@ -1,10 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 
-[HttpGet("")]
-public IActionResult GetAllModels() {
-    return Ok("This is GetAllModels function");
+
+namespace TinySoldiers.Controllers 
+{
+    public class ModelController : Controller 
+    {
+        [HttpGet("")]
+        public IActionResult GetAllModels() {
+            return Ok("This is GetAllModels function");
+        }
+
+        [HttpGet("model/{modelId}")]
+        public IActionResult GetModelById(int modelId) {
+            return Ok("This is model by id function");
+        }
+    }
+    
 }
 
-[HttpGet("model/{modelId}")]
-public IActionResult GetModelById(int modelId) {
-    return Ok("This is model by id function");
-}
--- Startup.cs
+++ Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace small_assignment_2
+namespace TinySoldiers
 {
     public class Startup
     {
