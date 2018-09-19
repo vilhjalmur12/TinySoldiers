2a59404de46e83e67d045f227bc998a6a3d28f87
villi <vilhjalmur12@ru.is>
2018-09-19 14:54:49 +0000

    tenging við DbContext

--- Controllers/ModelController.cs
+++ Controllers/ModelController.cs
@@ -1,15 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
+using TinySoldiers.Data;
+using TinySoldiers.Models;
+using TinySoldiers.Extensions;
 
 
 namespace TinySoldiers.Controllers 
 {
     public class ModelController : Controller 
     {
+
         [HttpGet("")]
         public IActionResult GetAllModels([FromQuery]int pageN = 1, [FromQuery]int pageS = 10) {
-            return Ok("This is GetAllModels function");
+
+            List<ModelDTO> tmp = ListExtensions.ToLightWeight(DataContext.Models);
+
+            return Ok(tmp);
         }
 
         [HttpGet("model/{modelId}")]
