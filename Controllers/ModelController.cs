1d55452c964c44096cf8ac24c0874f58c54511e8
villi <vilhjalmur12@ru.is>
2018-09-20 11:41:57 +0000

    link population and by id

--- Controllers/ModelController.cs
+++ Controllers/ModelController.cs
@@ -16,18 +16,36 @@ namespace TinySoldiers.Controllers
 
             List<ModelDTO> tmp = ListExtensions.ToLightWeight(DataContext.Models);
 
+            var tmp2 = DataContext.Models.ToDetails();
+
             var listItems = ListExtensions.ToLightWeight(DataContext.Models)
                 .OrderBy(c => c.Id)
-                .Skip(pageN)
+                .Skip((pageN - 1) * pageS)
                 .Take(pageS)
                 .ToList();
 
-            return Ok(new Envelope<ModelDTO>(listItems, pageS, pageN, 100));
+            LinkBuilder(ref listItems);
+            
+            return Ok(new Envelope<ModelDTO>(listItems, pageS, pageN, tmp.Count()));
         }
 
         [HttpGet("model/{modelId}")]
         public IActionResult GetModelById(int modelId) {
-            return Ok("This is model by id function ");
+            var _db = ListExtensions.ToLightWeight(DataContext.Models);
+
+            ModelDTO model = _db.FirstOrDefault(m => m.Id == modelId);
+
+            return Ok(model);
+        }
+
+        private void LinkBuilder(ref List<ModelDTO> modelList) {
+            
+            foreach(ModelDTO item in modelList)
+            {
+                item.Links.TryAdd("method", "GET");
+                var getUri = "http://localhost:5000/model/" + item.Id;
+                item.Links.TryAdd("uri", getUri);
+            }
         }
     }
     
